2c723f83d393c9dd56e0f3c8f67ba749915198bf
dotafs2 <qdotafs@gmail.com>
2024-09-16 18:31:02 +0800

    add grass

--- Assets/C#Cript/GrassInstancer.cs
+++ Assets/C#Cript/GrassInstancer.cs
@@ -4,35 +4,43 @@ using UnityEngine;
 
 public class GrassInstancer : MonoBehaviour
 {
-    public GameObject grassPrefab; // 草的预制件
-    public Material grassMaterial; // 草的材质
-    public int grassCount = 10; // 草的数量
-    public float areaSize = 1; // 草的分布范围大小
+    public GameObject grassPrefab;
+    public Material grassMaterial;
+    public int grassCount = 10; 
+    public float areaSize = 1f; 
 
     void Start()
     {
-        // 创建一个 GameObject 来容纳所有的草模型
         GameObject grassContainer = new GameObject("GrassContainer");
 
-        // 生成指定数量的草
-        for (int i = 0; i < grassCount; i++)
+        // 计算每个网格单元的大小
+        int gridCount = Mathf.CeilToInt(Mathf.Sqrt(grassCount));
+        float cellSize = areaSize / gridCount;
+
+        int placedGrass = 0;
+
+        for (int x = 0; x < gridCount; x++)
         {
-            // 实例化草的预制件
-            GameObject grass = Instantiate(grassPrefab, grassContainer.transform);
-            grass.transform.position = new Vector3(
-                Random.Range(-areaSize / 2, areaSize / 2),
-                0, 
-                Random.Range(-areaSize / 2, areaSize / 2)
-            );
-
-            // 随机旋转草
-            grass.transform.rotation = Quaternion.Euler(0, Random.Range(0, 360), 0);
-
-            // 设置材质
-            MeshRenderer mr = grass.GetComponent<MeshRenderer>();
-            if (mr != null)
+            for (int z = 0; z < gridCount; z++)
             {
-                mr.material = grassMaterial;
+                if (placedGrass >= grassCount)
+                    break;
+
+                // 计算草的位置，加上随机偏移
+                float posX = -areaSize / 2 + x * cellSize + Random.Range(0, cellSize);
+                float posZ = -areaSize / 2 + z * cellSize + Random.Range(0, cellSize);
+
+                GameObject grass = Instantiate(grassPrefab, grassContainer.transform);
+                grass.transform.position = new Vector3(posX, 0, posZ);
+                grass.transform.rotation = Quaternion.Euler(0, Random.Range(0, 360), 0);
+
+                MeshRenderer mr = grass.GetComponent<MeshRenderer>();
+                if (mr != null)
+                {
+                    mr.material = grassMaterial;
+                }
+
+                placedGrass++;
             }
         }
     }
